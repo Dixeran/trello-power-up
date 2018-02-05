dd5cffdb4e0981087ce4ae2dbe6b1024dc6969ce
dixeran <dixeran@outlook.com>
2018-02-05 23:09:55 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- js/mapbox.js
+++ js/mapbox.js
@@ -6,8 +6,13 @@ TrelloPowerUp.initialize({
             {
                 icon: './images/icon-white.svg',
                 text: 'Mapbox',
-                callback: function () {
+                callback: function (t) {
                     console.log("no return test");
+                    t.model({
+                        url: './index.html',
+                        accentColor: '#F2D600', // Optional color for the modal header 
+                        fullscreen: true
+                    });
                     return console.log("hahaha");
                 }
             }
