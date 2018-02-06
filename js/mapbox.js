dac1accd7bc81543caa04872dfb8534d504754b2
dixeran <dixeran@outlook.com>
2018-02-06 11:29:46 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,13 +1,13 @@
 var Promise = TrelloPowerUp.Promise;
 
 TrelloPowerUp.initialize({
+    // 看板右上角按钮
     'board-buttons': function (t, options) {
         return [
             {
                 icon: './images/icon-white.svg',
                 text: 'Mapbox',
                 callback: function (t) {
-                    console.log("no return test");
                     return t.modal({
                         url: './index.html',
                         accentColor: '#F2D600', // Optional color for the modal header 
@@ -16,5 +16,24 @@ TrelloPowerUp.initialize({
                 }
             }
         ]
+    },
+    // 卡片标签
+    'card-gadges': function (t) {
+        return t.card('name').get(name).then(function (CardName) {
+            console.log(CardName);
+            return new Promise(function (resolve) {
+                console.log('in Promise');
+                resolve([
+                    {
+                        text: 'test1',
+                        color: null
+                    },
+                    {
+                        text: 'test2',
+                        color: "green"
+                    }
+                ]);
+            });
+        })
     }
 });
